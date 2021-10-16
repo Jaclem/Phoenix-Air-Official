7d5c67f84895def2f11426576c749f562c7a844f
Jaclem <jaclem12@gmail.com>
2021-10-15 21:24:31 -0400

    Added Python Logic
    Added some python logic to add an error if not all fields were filled out on the contact page. I also added some extra (almost finalized) styles.

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    title = "Phoenix Air Services"
+    return render_template("index.html", title=title)
+
 
 @app.route("/contact-us")
 def contact():
-    return render_template("contact.html")
+    title = "Contact Us"
+    return render_template("contact.html", title=title)
+
+
+@app.route("/form", methods=["POST"])
+def form():
+    first_name = request.form.get("fname")
+    last_name = request.form.get("lname")
+    email = request.form.get("email")
+    phone_number = request.form.get("phone")
+    message = request.form.get("message")
+
+    if not first_name or not last_name or not email or not phone_number or not message:
+        error_statement = "All form fields required..."
+        return render_template("contact.html", 
+            error_statement=error_statement, first_name=first_name, 
+            last_name=last_name, 
+            email=email, 
+            phone_number=phone_number, 
+            message=message)
+
+    return render_template("form.html", 
+        first_name=first_name, 
+        last_name=last_name, 
+        email=email, 
+        phone_number=phone_number, 
+        message=message)
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
