eb634c1d631979ea8a18215e965355a3e165894d
Jaclem <jaclem12@gmail.com>
2021-10-24 22:07:01 -0400

    Tweaking
    Tweaking the website to look a little better, as well as added some media queries to make it more responsive.

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ import smtplib
 
 app = Flask(__name__)
 
-# app.config['MAIL_SERVER']='smtp.gmail.com'
-# app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USERNAME'] = 'Jaclem12@gmail.com'
-# app.config['MAIL_PASSWORD'] = 'ovutdwridtgtjqxu'
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USE_SSL'] = True
-
-# mail = Mail(app)
-
 @app.route("/")
 def home():
     title = "Phoenix Air Services"
@@ -26,38 +17,33 @@ def contact():
 
 @app.route("/form", methods=["POST"])
 def form():
-    first_name = request.form.get("fname")
-    last_name = request.form.get("lname")
-
-    full_name = first_name + " " + last_name
+    name = request.form.get("name")
 
     email = request.form.get("email")
     phone_number = request.form.get("phone")
     message = request.form.get("message")
 
-    customer_info = [full_name, email, phone_number, message]
+    customer_info = [name, email, phone_number, message]
     full_message = '\n\n'.join(customer_info)
 
     print(full_message)
     
-    company_email = "Jaclem12@gmail.com"
+    company_email = "bobpairserv@aol.com"
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login("Jaclem12@gmail.com","ovutdwridtgtjqxu")
-    server.sendmail("Jaclem12@gmail.com", company_email, full_message)
+    server.login("phoenixairhvac@gmail.com","G1Zw7jA7tzoZ")
+    server.sendmail("phoenixairhvac@gmail.com", company_email, full_message)
 
-    if not first_name or not last_name or not email or not phone_number or not message:
+    if not name or not email or not phone_number or not message:
         error_statement = "All form fields required..."
         return render_template("contact.html", 
-            error_statement=error_statement, first_name=first_name, 
-            last_name=last_name, 
+            error_statement=error_statement, name=name,
             email=email, 
             phone_number=phone_number, 
             message=message)
 
     success_statement = "Your message has been sent succesfully!"
-        
 
     return render_template("contact.html", success_statement=success_statement) 
 
