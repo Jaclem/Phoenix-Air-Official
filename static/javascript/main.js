468ad02f350e7e1ba59449731a4acb530e924177
Jaclem <jaclem12@gmail.com>
2021-10-27 12:36:18 -0400

    Update

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,16 +1,21 @@
 // created 3 constants that select the specific divs on main page to be faded
 
 const imageGallery = document.getElementById('img-gal');
+const outroMessage = document.getElementById('outro-msg');
 
 // function that adds a specific class to the argument that is passed to it
 function addClass(selector){
     selector.classList.add('afterScroll');
 }
 
+
 // event listener that finds the y-position and trigers events when it hits certain values
 window.addEventListener("scroll", (event) => {
 
     let scrollY = window.pageYOffset; // constant containing the y-position of page
     console.log(scrollY);
+
     if (scrollY >= 700){ addClass(imageGallery); }
+    if (scrollY >= 1750){ addClass(outroMessage); }
+
 });
